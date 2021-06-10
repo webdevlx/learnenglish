8dfaeb8bb26e747f448467a9aa8c7f27a6889c70
Lazizbek <webdevlx1005@gmail.com>
2021-06-10 18:21:13 +0500

    Components Pt.2 end

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>Dashboard</h1>
+    <v-data-table
+      :headers="headers"
+      :items="employees"
+      multi-sort
+      :items-per-page="5"
+      class="elevation-1"
+      @click:row="selectRow"
+    ></v-data-table>
+    <v-snackbar v-model="snackbar">
+      {{ currentRow.name }} - {{ currentRow.title }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+
+<script>
+import employees from '../data/employees.json'
+export default {
+  data() {
+    return {
+      snackbar: false,
+      currentRow: {},
+      headers: [
+        {
+          text: "Employee ID",
+          value: "id"
+        },
+        {
+          text: "Name",
+          value: "name"
+        },
+        {
+          text: "Position Title",
+          value: "title"
+        },
+        {
+          text: "Salary",
+          value: "salary"
+        }
+      ],
+      employees: employees
+    };
+  },
+  methods: {
+    selectRow(event) {
+      this.snackbar = true;
+      this.currentRow = event
+    },
+  },
+};
+</script>
+<style scoped>
+</style>
