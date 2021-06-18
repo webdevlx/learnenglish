d40d784d24c35ed88be1d4c44b807adb1a506f27
Lazizbek <webdevlx1005@gmail.com>
2021-06-18 18:45:12 +0500

    Forms - Validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -3,42 +3,68 @@
     <v-row>
       <v-col>
         <h1>Signup</h1>
-        <v-form>
+        <v-form ref="form" v-model="formValidity">
           <v-row>
             <v-col cols="12" sm="6" md="4">
-              <v-text-field label="Email" type="email"></v-text-field
-            ></v-col>
+              <v-text-field
+                label="Email"
+                type="email"
+                v-model="email"
+                :rules="emailRules"
+                required
+              >
+              </v-text-field>
+            </v-col>
 
             <v-col cols="12" sm="6" md="4">
               <v-autocomplete
                 label="Which browser do you use?"
                 :items="browsers"
-              ></v-autocomplete
-            ></v-col>
+                v-model="browser"
+                :rules="browserRules"
+              >
+              </v-autocomplete>
+            </v-col>
 
             <v-col cols="12" md="4">
-              <v-file-input label="Attach profile photo"></v-file-input
-            ></v-col>
+              <v-file-input label="Attach profile photo"></v-file-input>
+            </v-col>
 
             <v-col cols="12" sm="6">
-              <v-text-field
-                label="Birthday"
-                v-model="birthday"
-                readonly
-              ></v-text-field
-            ></v-col>
+              <v-text-field label="Birthday" v-model="birthday" :rules="birthdayRules" readonly>
+              </v-text-field>
+            </v-col>
 
             <v-col cols="12" sm="6">
-              <v-date-picker full-width elevation="1" v-model="birthday"></v-date-picker>
+              <v-date-picker full-width elevation="1" v-model="birthday">
+              </v-date-picker>
             </v-col>
 
             <v-col cols="12">
-              <v-checkbox label="Agree to terms & conditions"></v-checkbox
-            ></v-col>
+              <v-checkbox
+                label="Agree to terms & conditions"
+                v-model="agreeToTerms"
+                :rules="agreeToTermsRules"
+              >
+              </v-checkbox>
+            </v-col>
 
             <v-col cols="12">
-              <v-btn type="submit" color="primary">Submit</v-btn></v-col
-            >
+              <v-btn
+                class="mr-4"
+                :disabled="!formValidity"
+                type="submit"
+                color="primary"
+                >Submit</v-btn
+              >
+              <v-btn class="mr-4" color="success" @click="validateForm"
+                >Validate Form</v-btn
+              >
+              <v-btn class="mr-4" color="warning" @click="resetValidation"
+                >Reset Validation</v-btn
+              >
+              <v-btn color="error" @click="resetForm">Reset</v-btn>
+            </v-col>
           </v-row>
         </v-form>
       </v-col>
@@ -50,9 +76,43 @@
 export default {
   data() {
     return {
+      agreeToTerms: false,
+      agreeToTermsRules: [
+        (value) =>
+          !!value ||
+          "You must agree to the terms and conditions to sign up for an account.",
+      ],
       birthday: "",
+      birthdayRules: [(value) => !!value || "Select Birthday date."],
       browsers: ["Chrome", "Firefox", "Safari", "Opera", "Edge"],
+      email: "",
+      emailRules: [
+        (value) => !!value || "Email is required.",
+        // (value) => value.indexOf("@") !== 0 || "Email should have a username.",
+        // (value) => value.includes("@") || "Email should include an @ symbol.",
+        // (value) =>
+        //   value.indexOf(".") - value.indexOf("@") > 1 ||
+        //   "Email should contain a valid domain.",
+        // (value) =>
+        //   value.indexOf(".") <= value.length - 3 ||
+        //   "Email should contain a valid domain extension.",
+        (value) => /.+@.+\..+.+/.test(value) || "Email must be valid",
+      ],
+      browser: "",
+      browserRules: [(value) => !!value || "Select browser."],
+      formValidity: false,
     };
+  },
+  methods: {
+    resetForm() {
+      this.$refs.form.reset();
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation();
+    },
+    validateForm() {
+      this.$refs.form.validate();
+    },
   },
 };
 </script>
