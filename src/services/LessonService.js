8493eebad557649e02e9529f5a780b888f525b3f
Lazizbek <juniordev1005@gmail.com>
2021-07-13 23:00:56 +0500

    baseUrl change to  baseURL

--- src/services/LessonService.js
+++ src/services/LessonService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseUrl: "https://my-json-server.typicode.com/webdevlx/vuetify-dashboard",
+  baseURL: "https://my-json-server.typicode.com/webdevlx/vuetify-dashboard",
   // baseURL: "http://localhost:3000",
   withCredentials: false,
   headers: {
